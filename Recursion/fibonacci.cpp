e5de9edc678f87cfabdba5c3e6586476f3a22f19
Ritu Sharma <100413862+Ritu84@users.noreply.github.com>
2023-02-28 22:09:49 +0530

    Add files via upload

---
+++ Recursion/fibonacci.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int fib(int n){
+    if(n<=1) return n;
+
+    return fib(n-1)+fib(n-2);
+}
+
+int main(){
+    int  n;
+    cin>>n;
+    for(int i=0; i<n;i++) cout<<fib(i)<<" ";
+    // cout<<fib(n);
+
+    return 0;
+}
