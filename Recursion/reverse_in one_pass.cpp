6a812063d5d892f4ce481b21892669c528499ae2
Ritu Sharma <100413862+Ritu84@users.noreply.github.com>
2023-03-02 09:38:40 +0530

    Add files via upload

---
+++ Recursion/reverse_in one_pass.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverse(int i, int arr[], int n){
+    if(i>=n/2) return;
+
+    swap(arr[i], arr[n-i-1]);
+    reverse(i+1, arr, n);
+}
+
+int main(){
+    int n; cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+      reverse(0, arr, n);
+    for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+    
+    return 0;
+}
