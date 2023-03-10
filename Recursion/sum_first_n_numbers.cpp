6c6042c1478ecf40b15844e50b64c597377f3475
Ritu Sharma <100413862+Ritu84@users.noreply.github.com>
2023-03-10 23:10:25 +0530

    Add files via upload

---
+++ Recursion/sum_first_n_numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int  sum(int n){
+    return (n*(n+1))/2;
+}
+
+int main(){
+    int n=3;
+   cout<<sum(n);
+    
+    return 0;
+}
