c6471feb0668e6a6b130bda614f5bc6bde54a0ab
Ritu Sharma <100413862+Ritu84@users.noreply.github.com>
2023-03-19 22:58:17 +0530

    Add files via upload

---
+++ Searching/3.Index_first_occurence.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<unordered_set>
+#include<stack>
+#include<unordered_map>
+#include<bits/stdc++.h>
+using namespace std;
+
+int firstoccur(int arr[], int n, int x){
+    int low=0, high=n-1;
+
+    while(low<=high){
+        int mid= (low+high)/2;
+
+        if(arr[mid]<x) low=mid+1;
+
+        else if(arr[mid]>x) high=mid-1;
+
+        else{
+            if(mid==0 || arr[mid-1] != arr[mid]) return mid;
+
+            else high=mid-1;//go to left half
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[]={1, 3, 3, 4, 5};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    int x=3;
+
+    cout<<firstoccur(arr,n, x);
+
+    return 0;
+}
