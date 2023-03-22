e4c7ed02991d45d424dded9995698370b7c55db7
Ritu Sharma <100413862+Ritu84@users.noreply.github.com>
2023-03-22 23:30:25 +0530

    Add files via upload

---
+++ Bit-Manipulation/bitwise_not.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    unsigned int x=1;
+    cout<<(~x)<<"\n";
+
+    x=5;
+    cout<<(~x)<<"\n";
+
+    return 0;
+}
